e696ccdbdc6f8b96d6c030abca87abb8a69db8b6
rmallick6806 <rmallick6806@gmail.com>
2015-06-16 17:25:45 -0400

    Keyboard controls
    Added keyboard buttons

--- drum-logix.js
+++ drum-logix.js
@@ -2,6 +2,7 @@ var snare = Myo.create();
 var kick = Myo.create(1);
 
 
+
 snare.on('fist', function(edge){
     //Edge is true if it's the start of the pose, false if it's the end of the pose
     if(edge){
@@ -16,10 +17,7 @@ snare.on('imu', function(data){
 
 		pubsub.publish('hitDrum', 's');
 
-	// } else if(Math.abs(data.accelerometer.y)>2) {
-
-	// 	pubsub.publish('hitDrum', 's');
-	// }
+	
 	  }
 });
 
@@ -32,3 +30,39 @@ kick.on('imu', function(data){
         
     }
 });
+
+function myFunction(snareval) {
+    var snareval = document.getElementById("SnareChar").value;
+    var kickval = document.getElementById("KickChar").value;
+    var snarekey = snareval.keyCode;
+    //document.getElementById("demo").innerHTML = snareval;
+    alert(snareval.keyCode)
+}
+
+
+
+
+
+
+
+//Optional Keyboard Controls
+document.addEventListener('keypress', function(event) {
+    if (event.keyCode == 115) {
+       pubsub.publish('hitDrum', 's')
+    }
+   
+   	else if (event.keyCode == 98) {
+       pubsub.publish('hitDrum', 'k')
+    }
+
+    else if (event.keyCode == 104) {
+        pubsub.publish('hitDrum', 'hh')
+    }
+
+    else if (event.keyCode == 99) {
+        pubsub.publish('hitDrum', 'c')
+    }
+
+}, true);
+
+
